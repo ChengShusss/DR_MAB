55b699690a2651b0ac2c51bd4774aabb5f84d208
ShadowCheng <chengshu_seu@163.com>
2019-06-14 23:54:20 +0800

    add several method in MAB_method.py

--- MAB_method.py
+++ MAB_method.py
@@ -3,3 +3,18 @@ __author__ = 'S.CHENG'
 
 from user import *
 import numpy as np
+
+def riskAverse(userL, target):
+	signal = [0]*len(userL)
+	pass
+	return signal
+
+def CUCBAVG(userL, target):
+	signal = [0]*len(userL)
+	pass
+	return signal
+
+def conveMethod(userL, target):
+	signal = [0]*len(userL)
+	pass
+	return signal
