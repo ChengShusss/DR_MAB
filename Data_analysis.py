10e3d9e683ff94c951d468654448e07921cb1381
ShadowCheng <chengshu_seu@163.com>
2019-07-05 09:23:57 +0800

    cancel the compansation of conventional

--- Data_analysis.py
+++ Data_analysis.py
@@ -35,6 +35,8 @@ def plotAll(decData, optData, userData, colorData, info):
 		#fig of reduction
 		ax1.cla()
 		ax1.set_xlim(axisLim[0][0], axisLim[0][1])
+		ax1.set_xlabel("Time", fontdict=fontdic, size=15, x= 0.95, y=-0.2)
+		ax1.set_ylabel("Real reduction", fontdict=fontdic, size=15)
 		ax1.set_ylim(axisLim[1][0], axisLim[1][1])
 		ax1.set_title("Actual Reduction", fontdict=fontdic)
 		I11 = ax1.scatter(X1[0:i+1], decData.iloc[1,0:i+1], s=35, marker='2',
@@ -48,6 +50,8 @@ def plotAll(decData, optData, userData, colorData, info):
 		ax2.cla()
 		ax2.set_xlim(axisLim[2][0], axisLim[2][1])
 		ax2.set_ylim(axisLim[3][0], axisLim[3][1])
+		ax2.set_xlabel("Actual Prob distribute", fontdict=fontdic, size='small')
+		ax2.set_ylabel("Thought Prob distribute", fontdict=fontdic, size='small')
 		ax2.set_title("Knowledge of Users", fontdict=fontdic)
 		colorSet = colorData.iloc[i].tolist()
 		ax2.scatter(userX, userData.iloc[i+1].tolist(), s=1, color=colorSet)
@@ -57,6 +61,8 @@ def plotAll(decData, optData, userData, colorData, info):
 		ax3.cla()
 		ax3.set_xlim(axisLim[4][0], axisLim[4][1])
 		ax3.set_ylim(axisLim[5][0], axisLim[5][1])
+		ax3.set_xlabel("Time", fontdict=fontdic, size='small')
+		ax3.set_ylabel("User opt-outs", fontdict=fontdic, size='small')
 		ax3.set_title("The count of user opt-out", fontdict=fontdic)
 		#ax3.scatter(X21, Y21 , s=1, color=color2)
 		ax3.plot(X1[0:i+1], optData.iloc[0, 0:i+1], color='r')
@@ -67,3 +73,9 @@ def plotAll(decData, optData, userData, colorData, info):
 		plt.pause(0.001)
 
 	plt.show()		#to retain image
+
+
+
+
+
+#subplot loaction adjustment: https://www.zhihu.com/question/21953954
